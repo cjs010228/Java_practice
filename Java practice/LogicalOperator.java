97627a77714e6334ce9889b5d92c5c2cd99da7dd
cjs010228 <cjs010228@gmail.com>
2014-12-24 17:28:26 +0800

    Created by Coding Ground Client

---
+++ Java practice/LogicalOperator.java
@@ -0,0 +1,7 @@
+public class LogicalOperator{
+  public static void main(String args[]){
+      int num = 100;
+      System.out.printf("%e ~ %e\n",Double.MAX_VALUE,Double.MIN_VALUE);
+      System.out.println(80>num && num<70);
+  }
+}
